b33d165bae0ee8afe6ec37b8a2cd2bea226bfb31
Xu Lyu <cslyuxu@comp-dept-ad2.comp.hkbu.edu.hk>
2020-10-04 00:45:54 +0800

    wrong push

--- PP-StrongSimulation/experiment script/ball count/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#$1 for labelsize
-
-g++ main.cpp -o main -std=c++17
-
-./main D3-64-runtime
-
--- PP-StrongSimulation/experiment script/ball interval count/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#$1 for labelsize
-
-g++ main.cpp -o main -std=c++17
-
-./main S3-64-runtime 2000 8 3 5
-
